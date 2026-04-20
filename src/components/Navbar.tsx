d5798e3be9852299201a88a001b7222b5ab9706a
kachiDeGreat <mail.ricx@gmail.com>
2026-04-20 12:30:06 +0100

    feat: design and implement responsive features page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ export default function Navbar() {
       </Link>
 
       <div className={styles.navLinks}>
-        <a href="#features" className={styles.link}>
+        <Link to="/features" className={styles.link}>
           Features
-        </a>
+        </Link>
         <Link to="/templates" className={styles.link}>
           Templates
         </Link>
@@ -27,10 +27,7 @@ export default function Navbar() {
 
       <div className={styles.actions}>
         <button className={styles.loginBtn}>Log in</button>
-        <button 
-          className={styles.ctaBtn} 
-          onClick={() => navigate('/editor')}
-        >
+        <button className={styles.ctaBtn} onClick={() => navigate("/editor")}>
           Go to Editor
         </button>
       </div>
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -2,13 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage';
 import TemplatesPage from '../pages/TemplatesPage';
+import FeaturesPage from '../pages/FeaturesPage';
 
 const Index = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/templates" element={<TemplatesPage />} />
-      {/* <Route path="/editor" element={<EditorPage />} /> */}
+      <Route path="/features" element={<FeaturesPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
